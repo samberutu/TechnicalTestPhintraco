b4593648985df205f5efd5234b396a8a8ed5b250
samberutu <45347400+samberutu@users.noreply.github.com>
2023-03-03 19:30:04 +0700

    Initial Commit

---
+++ Technical Test Phintraco/Technical_Test_PhintracoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Technical_Test_PhintracoApp.swift
+//  Technical Test Phintraco
+//
+//  Created by Samlo Berutu on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Technical_Test_PhintracoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
